076c9d00b24e2f9bfd12c89cae15c3e6795b1ab2
Alberto Luebbert M <albertoluebbert@ideashappy.com>
2017-06-03 11:16:26 -0500

    Agrega instruccion para permitir migrates con cadenas largas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,8 @@
 <?php
 
+//NameSpaces
+//Clases en Laravel para utilizarlo en toda
+//la clase
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -13,6 +16,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        //Crearemos una tabla con un capo incrementable llamado id
+        //donde tendremos un campo string de tipo nombre
+        // y un campo de tipo email donde ademas debe ser de tipo unico
+        // para que no se repita en toda la plataforma
+        // timestamps todos los registros tendran fecha de creación y fecha de actualizacion
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
@@ -22,6 +30,11 @@ class CreateUsersTable extends Migration
             $table->timestamps();
         });
     }
+
+    //cuando ejecutemos el archivo de migrates
+    //quiero deshacer cambios en la tabla / base de datos
+    // esta funcion (down) hara una función tipo rollback
+    // deshace nuestra utlima accion
 
     /**
      * Reverse the migrations.
